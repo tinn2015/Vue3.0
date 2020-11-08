929e5f68b9e0eaccee2a76e581b8ad75b1c1b80a
tinn <229813475@qq.com>
2020-11-08 10:42:55 +0800

    初始化

---
+++ demo/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <div class="cell"><RefReactive></RefReactive></div>
+    <div class="cell"></div>
+  </div>
+</template>
+
+<script>
+import RefReactive from './components/RefReactive.vue'
+
+export default {
+  name: 'App',
+  components: {
+    RefReactive
+  }
+}
+</script>
